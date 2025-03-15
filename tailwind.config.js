c538188958400b1fa43009f8b10daf8ea41c4657
aniket30i <iamaniket.saha.09@protonmail.com>
2025-03-15 20:00:27 +0530

    custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: "#d6c6ff",
+          200: "#A8b5db",
+          300: "#9ca4ab",
+        },
+        dark: {
+          100: "#221f3d",
+          200: "#0f0d23",
+        },
+        accent: "#AB8BFF",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
