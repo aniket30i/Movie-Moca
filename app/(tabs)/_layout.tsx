d4a82ca1b79d274ee73f5509622c2d31b83445fb
aniket30i <iamaniket.saha.09@protonmail.com>
2025-03-17 14:02:47 +0530

    layouts routes and tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,24 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+
+const _layout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{ headerShown: false, title: "Home" }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{ headerShown: false, title: "Search" }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{ headerShown: false, title: "Saved" }}
+      />
+    </Tabs>
+  );
+};
+
+export default _layout;
