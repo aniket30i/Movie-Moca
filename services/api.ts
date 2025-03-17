722993f3fe1cfa96d388dbdcc33ad5a7cac8bf8c
aniket30i <iamaniket.saha.09@protonmail.com>
2025-03-17 20:01:31 +0530

    search and router

---
+++ services/api.ts
@@ -0,0 +1,13 @@
+// const url = 'https://api.themoviedb.org/3/keyword/avengers/movies?include_adult=true&language=en-US&page=1';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZTI2OGQ5NzU1MzgyYjI2NDc5ODZlYmM0MmFiZjBhZCIsIm5iZiI6MTc0MjIyMDc4NS4wMjQsInN1YiI6IjY3ZDgyZGYxMzE1MzhkZTYwOGYxZWIyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZBkgf5KR4dAUDPGD1ouIlI1Yw4hg5ZEzs1kU88-J_jc'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
