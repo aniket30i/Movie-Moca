fcb779bbb560f1dfbb452d7436d9de355846ec28
aniket30i <iamaniket.saha.09@protonmail.com>
2025-03-18 20:17:28 +0530

    listing of movies

--- services/api.ts
+++ services/api.ts
@@ -9,31 +9,19 @@ export const TMDB_CONFIG = {
 
 export const fetchMovies = async ({ query }: { query: string }) => {
   const endpoint = query
-    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURI(query)}`
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
     : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
   const response = await fetch(endpoint, {
     method: "GET",
     headers: TMDB_CONFIG.headers,
   });
-
+  console.log("api res", response);
   if (!response.ok) {
-    throw new Error("Failed to fetch movies", response.statusText);
+    throw new Error("Failed to fetch movies", { cause: response.statusText });
   }
 
   const data = await response.json();
+
   return data.results;
 };
-// const url = 'https://api.themoviedb.org/3/keyword/avengers/movies?include_adult=true&language=en-US&page=1';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZTI2OGQ5NzU1MzgyYjI2NDc5ODZlYmM0MmFiZjBhZCIsIm5iZiI6MTc0MjIyMDc4NS4wMjQsInN1YiI6IjY3ZDgyZGYxMzE1MzhkZTYwOGYxZWIyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZBkgf5KR4dAUDPGD1ouIlI1Yw4hg5ZEzs1kU88-J_jc'
-//   }
-// };
-
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
